13eac7b8fe82a1d0347895b2a58e2aa3d07d268e
Pabearyoga <pabearyoga@gmail.com>
2023-03-03 20:46:56 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,29 @@
-import { useState } from "react";
-import Statistics from "./Statistics/Statistics";
-import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
-import Section from "./Section/Section";
-import Notification from "./Notification/Notification";
+// import { useState } from "react";
 
-
+import bgImg from '../picture.svg'
+import logo from '../Vector.svg'
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const optionFeedback = { good, neutral, bad }
-  
-  const  addFeedback = event => {
-    switch (event) {
-      case 'good':
-            setGood((prevState) => (prevState + 1))
-        break;
-      case 'neutral':
-            setNeutral((prevState) => (prevState + 1))
-        break;
-      case 'bad':
-            setBad((prevState) => (prevState + 1))
-        break;
-      default:
-        break;
-        }
-  };
-
-  const countTotalFeedback = () => {
-    return Object.values(optionFeedback).reduce((acc, option) => acc + option, 0)  }
-
-  const countPositiveFeedbackPercentage = () => {
-    const positivePercent = (100 / countTotalFeedback() * good)
-    return positivePercent ? Math.round(positivePercent) : 0
-  }
-
 
 
   return (
-    <div>
-        <Section title="Please leave feedback" >
-          <FeedbackOptions
-            options={Object.keys(optionFeedback)}
-            onLeaveFeedback={addFeedback}
-            />
-        </Section>
+    <div style={{
+      display: "flex", justifyContent: 'center', paddingTop: 10} }>
+      <div style={{ width: 380, height: 460, borderRadius: 20, background: ' linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%)', position: 'relative' }}>
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: 28}}>
+          <img src={bgImg} alt=""  />
+        </div>
+        <div style={{ position: 'absolute', top: 20, left: 20}}>
+          <img src={logo} alt=""  />
+        </div>
+        <div style={{marginBottom: 16, display: 'flex', justifyContent: 'center', textTransform: 'uppercase', color: '#EBD8FF'}}>
+          <p>777 tweets</p>
+        </div>
+        <div style={{marginBottom: 16, display: 'flex', justifyContent: 'center', textTransform: 'uppercase', color: '#EBD8FF'}}>
+          <p>100,500 followers</p>
+        </div>
+        
 
-
-      <Section title="Statistick">
-        {countTotalFeedback() > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={countTotalFeedback()}
-          positivePercentage={countPositiveFeedbackPercentage()}
-            />) : (<Notification message="There is no feedback"/>)}
-        </Section>
+      </div>
       </div>
 )
 }
