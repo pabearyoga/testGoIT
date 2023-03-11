907f90a1456ff984b465813457520fc53518740e
Pabearyoga <pabearyoga@gmail.com>
2023-03-11 10:14:03 +0200

    fix avatarBgLine (::after)

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -4,10 +4,9 @@ import css from "./Avatar.module.css"
 export default function Avatar({ img }) {
     return (
         <div className={css.avatarContainer}>
-            <div className={css.avatarBgLine}></div>
             <div className={css.avatarBorder}>
                 <div className={css.avatarBg}>
-                    <img src={img} alt="" className={css.avatarImg} />
+                    <img src={img} alt="avatar" className={css.avatarImg} />
                 </div>
 
             </div>
