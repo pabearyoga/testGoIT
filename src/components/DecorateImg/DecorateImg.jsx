99c74f3a2400f1444b5e583c1ec8f05603d07868
Pabearyoga <pabearyoga@gmail.com>
2023-03-11 10:18:38 +0200

    fix bgImg

--- src/components/DecorateImg/DecorateImg.jsx
+++ src/components/DecorateImg/DecorateImg.jsx
@@ -1,10 +1,8 @@
-import bgImg from '../../image/picture.svg'
 import css from "./DecorateImg.module.css"
 
 export default function DecorateImg() {
  return (
     <div className={css.imgWrapper}>
-         <img src={bgImg} alt="background" />
     </div>
     );
 }
