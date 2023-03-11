d94844ab59aced57f3c18704dbae1e2aca08d60c
Pabearyoga <pabearyoga@gmail.com>
2023-03-11 11:01:29 +0200

    add assets

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,4 +1,4 @@
-import logo from '../../image/Vector.svg'
+import logo from '../../assets/image/Vector.svg'
 import css from "./Logo.module.css"
 
 export default function Logo() {
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "components/App";
 import "./index.css";
-import "./fonts/Montserrat/static/Montserrat-Medium.ttf";
-import "./fonts/Montserrat/static/Montserrat-SemiBold.ttf";
+import "./assets/fonts/Montserrat/static/Montserrat-Medium.ttf";
+import "./assets/fonts/Montserrat/static/Montserrat-SemiBold.ttf";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
